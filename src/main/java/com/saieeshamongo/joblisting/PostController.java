8f4e0ca426739c91f9c6345a5381a06075293912
saieesha <saieeshabonthala@gmail.com>
2024-08-29 23:11:00 -0500

    Connect to swagger - unsuccessful

---
+++ src/main/java/com/saieeshamongo/joblisting/PostController.java
@@ -0,0 +1,15 @@
+package com.saieeshamongo.joblisting;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class PostController {
+    @RequestMapping(value="/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+}
